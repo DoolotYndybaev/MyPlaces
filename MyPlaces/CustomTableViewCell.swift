855a48728ed1784a818acb0a49e7850b0fd5a01f
DoolotYndybaev <91968210+DoolotYndybaev@users.noreply.github.com>
2022-08-09 00:59:09 +0600

    CustomViewCell

---
+++ MyPlaces/CustomTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CustomTableViewCell.swift
+//  MyPlaces
+//
+//  Created by Doolot on 9/8/22.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var imageOfPlace: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!
+}

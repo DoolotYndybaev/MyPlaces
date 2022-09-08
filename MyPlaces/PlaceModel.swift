bf82849d2750ab0f6a9bd6171fcb5a5354ef156f
DoolotYndybaev <91968210+DoolotYndybaev@users.noreply.github.com>
2022-09-08 19:52:28 +0600

    Pass Data between Controllers and Action Sheet Customization and Image selection

--- MyPlaces/PlaceModel.swift
+++ MyPlaces/PlaceModel.swift
@@ -6,12 +6,14 @@
 //
 
 import Foundation
+import UIKit
 
 struct Place {
     var name: String
-    var location: String
-    var type: String
-    var image: String
+    var location: String?
+    var type: String?
+    var image: UIImage?
+    var restaurantImage: String?
     
     static let restaurantNames = ["Burger Heroes", "Kitchen", "Bonsai", "Дастархан", "X.O", "Sherlock Holmes", "Speak Easy", "Morris Pub", "Love&Life", "Индокитай", "Балкан Гриль", "Вкусные истории", "Классик", "Шок", "Бочка"
     ]
@@ -20,7 +22,7 @@ struct Place {
         var places = [Place]()
         
         for place in restaurantNames {
-            places.append(Place(name: place, location: "Ufa", type: "Restoraunt", image: place))
+            places.append(Place(name: place, location: "Ufa", type: "Restoraunt", image: nil, restaurantImage: place))
         }
         return places
     }
